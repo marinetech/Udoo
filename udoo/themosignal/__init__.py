17b2209d5e663d84dac72622e7de5c762ca8ecbe
Roberto Francescon <francescon.roberto@gmail.com>
2016-06-23 12:27:53 +0200

    Correted init and README files

--- udoo/themosignal/__init__.py
+++ udoo/themosignal/__init__.py
@@ -2,3 +2,8 @@ import sys
 from themosignal import Signal
 from themosignal import Wave
 from themosignal import Recording
+from themosignal import Chirp
+from themosignal import Sine
+from themosignal import Rect
+from themosignal import writeToWAV
+from themosignal import writeToCSV
